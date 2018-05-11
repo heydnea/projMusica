028e357c474b0a3b5a9e1fb88e9ca1e4be64a416
Heydnea <heydnea_oliveira@hotmail.com>
2018-05-10 21:43:56 -0300

    Proj Musica

---
+++ src/main/java/com/projMusica/repository/MusicaRepository.java
@@ -0,0 +1,10 @@
+package com.projMusica.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.projMusica.model.Musica;
+
+	
+	public interface MusicaRepository extends CrudRepository<Musica, Long> {
+
+}
